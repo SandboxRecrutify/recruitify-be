2e64af520baa7d7e754d0374947734754c6fe207
alesyalis <lis.alesia@mail.ru>
2021-10-18 12:37:46 +0300

    fixd .editorconfig

--- Recrutify/Recrutify.Host/Controllers/WeatherForecastController.cs
+++ Recrutify/Recrutify.Host/Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Recrutify.Host.Controllers
 {
@@ -13,7 +12,7 @@ namespace Recrutify.Host.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
