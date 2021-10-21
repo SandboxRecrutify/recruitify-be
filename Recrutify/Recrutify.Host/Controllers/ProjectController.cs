39ed10c653983135c461c7a88bd48f1d244a8a82
alesyalis <lis.alesia@mail.ru>
2021-10-21 12:26:14 +0300

    rename CourseController into ProjectController

--- Recrutify/Recrutify.Host/Controllers/ProjectController.cs
+++ Recrutify/Recrutify.Host/Controllers/ProjectController.cs
@@ -9,24 +9,24 @@ namespace Recrutify.Host.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CourseController : ControllerBase
+    public class ProjectController : ControllerBase
     {
         private readonly IProjectService _courseService;
 
-        public CourseController(IProjectService courseService)
+        public ProjectController(IProjectService courseService)
         {
             _courseService = courseService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Project>>> GetAsync()
+        public async Task<ActionResult<List<ProjectDTO>>> GetAsync()
         {
             var result = await _courseService.GetAllAsync();
             return Ok(result);
         }
 
         [HttpPost]
-        public async Task<ActionResult> AddCourse(ProjectCreateDTO courseDto)
+        public async Task<ActionResult<ProjectDTO>> AddCourse(ProjectCreateDTO courseDto)
         {
             var result = await _courseService.CreateAsync(courseDto);
             return Created(string.Empty, result);
