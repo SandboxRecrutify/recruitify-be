e303ac20c31da046e596a36c933c7976b970a650
ProkopenkovD <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-10-21 18:36:13 +0300

    Create CandidateRepositore and CandidateService

---
+++ Recrutify/Recrutify.DataAccess/Repositories/CandidateRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Recrutify.DataAccess.Repositories.Abstract;
+
+namespace Recrutify.DataAccess.Repositories
+{
+    class CandidateRepository : BaseRepository<Candidate>, ICandidateRepository
+    {
+    }
+}
