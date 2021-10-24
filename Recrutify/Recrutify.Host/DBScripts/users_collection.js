033a3c86d7a1ae1f4dbebeb423309620279b9f3e
Evgen <evgentik_m@mail.ru>
2021-10-25 00:48:28 +0300

    Divide the script into collections, English descriptions

---
+++ Recrutify/Recrutify.Host/DBScripts/users_collection.js
@@ -0,0 +1,74 @@
+﻿var users = [
+        {
+            "_id": UUID("ce33e6c4-30ac-11ec-8d3d-0242ac130003"),
+            "Login": "alexanderson@gmail.com",
+            "Name": "Alex",
+            "Password": "2B8358C34FFFE78D302F826F54A4E9D92BDB657F6DFE01FA3FA22FA89D3347DB",
+            "Roles": [NumberInt(0), NumberInt(1)],
+            "Salt": "E1F53135E559C253",
+            "Surname": "Anderson"
+        },
+        {
+            "_id": UUID("73e05563-6c9e-4727-9ebb-b08f16cc1001"),
+            "Login": "anthonyclark@gmail.com",
+            "Name": "Anthony",
+            "Password": "646DAE47260A070B28D5E9727BA0D8A6A08CB6BF627602BE4E9229C5B7DAEAE2",
+            "Roles": [NumberInt(1), NumberInt(2)],
+            "Salt": "84B03D034B409D4E",
+            "Surname": "Clark"
+        },
+        {
+            "_id": UUID("6f885f2c-b60d-4743-b381-4e841d48a956"),
+            "Login": "brandonharris@gmail.com",
+            "Name": "Brandon",
+            "Password": "25077B4E7F514A5FB3E04C662965AA9F0DF15D1FE0384526F608E3A6733D973F",
+            "Roles": [NumberInt(2)],
+            "Salt": "RET4356433DFG345",
+            "Surname": "Harris"
+        },
+        {
+            "_id": UUID("d1b13d9f-3799-456b-87e2-1bef7db71cb1"),
+            "Login": "christopherjohnson@gmail.com",
+            "Name": "Christopher",
+            "Password": "31144862B60C43C5F045EAAC4D99E9F59DA3CFDAE42B372704D7C458B1E6DA8A",
+            "Roles": [NumberInt(4), NumberInt(3)],
+            "Salt": "SDFGHJ54645756FH",
+            "Surname": "Johnson"
+        },
+        {
+            "_id": UUID("4f04e857-50ec-42d7-8fe4-0e3d3d8d34b3"),
+            "Login": "davidlewis@gmail.com",
+            "Name": "David",
+            "Password": "C9A1CB6FB1D59764C8B12975E7B156D417141ED07832808F8F489064DDE3DBF8",
+            "Roles": [NumberInt(4), NumberInt(3)],
+            "Salt": "FDG4354GFD345542",
+            "Surname": "Lewis"
+        },
+        {
+            "_id": UUID("cb40d31e-7447-4ec0-bc53-ab566f3a7b2e"),
+            "Login": "fredtaylor@gmail.com",
+            "Name": "Fred",
+            "Password": "A606B0C8919A0F6EB85240460D6772AA963669688C2CBD0D4A0B329CA238D8B0",
+            "Roles": [NumberInt(3)],
+            "Salt": "234FGHDS234EG454",
+            "Surname": "Taylor"
+        },
+        {
+            "_id": UUID("71da4db7-36ec-4a6b-bfb3-38d2462bdf2d"),
+            "Login": "justinwalker@gmail.com",
+            "Name": "Justin",
+            "Password": "2B8CB1313018D3FAE4A5A58E2231AF82EFB6BE435574742A728AA7851615976F",
+            "Roles": [NumberInt(3)],
+            "Salt": "65734FHGFG54634G",
+            "Surname": "Walker"
+        },
+        {
+            "_id": UUID("03699464-c0a6-412f-8c83-eca85b359e91"),
+            "Login": "kevingarcia@gmail.com",
+            "Name": "Kevin",
+            "Password": "4B0BEB558891202DF7BC320DB22F8C7930BA3899D433B57A8E862CA42AF08305",
+            "Roles": [NumberInt(2)],
+            "Salt": "AS567TYTT4535RFH",
+            "Surname": "Wils"
+        }
+    ]
