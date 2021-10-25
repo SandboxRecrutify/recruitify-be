43f3fae550a5c2b65f50e603841a565f1c764eee
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-10-25 17:24:21 +0300

    Create constructor CandidateService

--- Recrutify/Recrutify.Services/Services/CandidateService.cs
+++ Recrutify/Recrutify.Services/Services/CandidateService.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.AccessData.Repositories;
 using Recrutify.Services.Services.Abstract;
 
 namespace Recrutify.Services.Services
 {
     class CandidateService : ICandidateService
     {
+        private readonly ICandidateRepository _candidateRepository;
+        private readonly IMapper _mapper;
+        public CandidateService(ICandidateRepository candidateRepository, IMapper mapper)
+        {
+            _candidateRepository = candidateRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<Candidate>> GetAllAsync()
+        {
+
+        }
     }
 }
