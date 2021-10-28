fd8a02749789b407bdf28ea3ad5d124f86a1f961
alesyalis <lis.alesia@mail.ru>
2021-10-28 20:16:23 +0300

    add Candidats DTO

---
+++ Recrutify/Recrutify.Services/DTOs/ContactDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Recrutify.Services.DTOs
+{
+    public class ContactDTO
+    {
+        public string Type { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Recrutify/Recrutify.Services/DTOs/FeedbackDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Recrutify.Services.DTOs
+{
+    public class FeedbackDTO
+    {
+        public string TextFeedback { get; set; }
+
+        public int Rating { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public FeedbackTypeDTO Type { get; set; }
+
+        public bool IsRecommended { get; set; }
+    }
+}
