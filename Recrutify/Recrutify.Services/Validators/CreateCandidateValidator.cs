3f39dec37140100530479a5ff255c728907a2d5e
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-10-28 20:48:42 +0300

    Create BaseCandidateValidator and CreateCandidateValidator empty

---
+++ Recrutify/Recrutify.Services/Validators/CreateCandidateValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Recrutify.Services.DTOs;
+
+namespace Recrutify.Services.Validators
+{
+    public class CreateCandidateValidator : BaseCandidateValidator<CandidateCreateDTO>
+    {
+    }
+}
