a813a82c9372567714f447ba48195e88dbc92d99
alesyalis <lis.alesia@mail.ru>
2021-10-28 10:20:03 +0300

    add folder Extensions , add Registration Repositorie, Registration Service, Registration Validaror

---
+++ Recrutify/Recrutify.Services/Extensions/RegistrationService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Recrutify.Services.Services;
+using Recrutify.Services.Services.Abstract;
+
+namespace Recrutify.Services.Extensions
+{
+    public static class RegistrationService
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProjectService, ProjectService>();
+            services.AddSingleton<ICandidateService, CandidateService>();
+            services.AddSingleton<IPrimarySkillService, PrimarySkillService>();
+        }
+    }
+}
