aae097f35dd5b58dbe007be0d24f131a9a7de8c8
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-10-29 22:22:03 +0300

    Add  not null

--- Recrutify/Recrutify.Services/Validators/BaseCandidateValidator.cs
+++ Recrutify/Recrutify.Services/Validators/BaseCandidateValidator.cs
@@ -3,10 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
+using Recrutify.Services.DTOs;
 
 namespace Recrutify.Services.Validators
 {
-    class BaseCandidateValidator
+    public abstract class BaseCandidateValidator<TDTO> : AbstractValidator<TDTO>
+        where TDTO : CandidateCreateDTO
     {
+        public BaseCandidateValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(128);
+            RuleFor(c => c.Surname)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(128);
+            RuleFor(c => c.EnglishLevel)
+                .NotNull();
+            RuleFor(c => c.PhoneNumber)
+                .NotNull()
+                .NotEmpty();
+            // проверка на отсутсвие букв
+            RuleFor(c => c.Email)
+               .NotNull()
+               .NotEmpty();
+            // проверка на соответствие электронной почте
+            RuleFor(c => c.Contacts)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.Location)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.PrimarySkills)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.RegistrationDate)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.BestTimeToConnect)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.GoingToExadel)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.ProjectResults)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.CurrentJob)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.Certificates)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.AdditionalQuestions)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.AdditionalInfo)
+                .NotNull();
+
+        }
     }
 }
