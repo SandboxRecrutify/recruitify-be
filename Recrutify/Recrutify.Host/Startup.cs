1db51f0bcda11f7898947d36ef9351a9a5393583
Ilya <wanne27@mail.ru>
2021-10-29 12:53:44 +0300

    commit

--- Recrutify/Recrutify.Host/Startup.cs
+++ Recrutify/Recrutify.Host/Startup.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using IdentityServer4.AccessTokenValidation;
