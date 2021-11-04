a3e669720e3f8b956e86973ec1df7919c42bce96
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-11-04 18:03:10 +0300

    add

--- Recrutify/Recrutify.Services/Validators/CreateCandidateValidator.cs
+++ Recrutify/Recrutify.Services/Validators/CreateCandidateValidator.cs
@@ -1,13 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 using Recrutify.Services.DTOs;
 
 namespace Recrutify.Services.Validators
 {
-    public class CreateCandidateValidator : BaseCandidateValidator<CandidateCreateDTO>
+    public abstract class CreateCandidateValidator<TDTO> : AbstractValidator<TDTO>
+        where TDTO : CandidateCreateDTO
     {
+        public CreateCandidateValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(50);
+            RuleFor(c => c.Surname)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(50);
+            RuleFor(c => c.PhoneNumber)
+                .NotNull()
+                .NotEmpty()
+                .Matches(@"^\+?(\d[\d\- ]+)?(\(?[\d\- ]+\))?([\d\- ])+\d$");
+            RuleFor(c => c.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress();
+            RuleFor(c => c.Contacts)
+                .NotNull()
+                .NotEmpty();
+            RuleForEach(c => c.Contacts)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.Location)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.PrimarySkills)
+                .NotNull()
+                .NotEmpty();
+            RuleForEach(c => c.PrimarySkills)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(c => c.BestTimeToConnect)
+                .NotNull()
+                .NotEmpty();
+            RuleForEach(c => c.BestTimeToConnect)
+                .NotEmpty();
+        }
     }
 }
