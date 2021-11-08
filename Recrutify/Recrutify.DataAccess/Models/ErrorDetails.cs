935283a7ad6723e91aa63e8f6522917b7ac57834
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-11-08 15:43:10 +0300

    Create ErrorDetails.cs

---
+++ Recrutify/Recrutify.DataAccess/Models/ErrorDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Recrutify.DataAccess.Models
+{
+    public class ErrorDetails
+    {
+        public int StatusCorde { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
