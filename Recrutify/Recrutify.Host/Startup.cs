c998b8366b565cdda42460cb640a068d83cf422e
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-11-11 12:53:40 +0300

    Add system for log file

--- Recrutify/Recrutify.Host/Startup.cs
+++ Recrutify/Recrutify.Host/Startup.cs
@@ -157,8 +157,7 @@ namespace Recrutify.Host
 
             app.UseCors(Constants.Cors.CorsForUI);
 
-            Log4NetConfig.SetConfiguration();
-            //loggerFactory.AddLog4Net();
+            loggerFactory.AddLog4Net();
 
             app.UseHttpsRedirection();
             app.UseRouting();
