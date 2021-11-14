ac0129550a115adf396b7a672552523c8554081e
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-11-14 20:15:04 +0300

    Add EmailService, EmailRequest, MailSettings

---
+++ Recrutify/Recrutify.Services/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Recrutify.DataAccess.Models;
+using Recrutify.Services.Services.Abstract;
+using Recrutify.DataAccess.Configuration;
+using Microsoft.Extensions.Options;
+using MimeKit;
+//using System.Net.Mail;
+using MailKit.Security;
+using MailKit.Net.Smtp;
+
+namespace Recrutify.Services.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(EmailRequest emailRequest)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailRequest.ToEmail));
+            email.Subject = emailRequest.Subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = emailRequest.Body };
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
