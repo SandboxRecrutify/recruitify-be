8f61b772d0f671c041c74b91b4872038c3fbf575
alesyalis <lis.alesia@mail.ru>
2021-11-16 18:21:01 +0300

    add other dto

--- Recrutify/Recrutify.Host/Controllers/OData/ProjectsController.cs
+++ Recrutify/Recrutify.Host/Controllers/OData/ProjectsController.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNet.OData.Routing;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Recrutify.Host.Infrastructure.CustomsAuthorizationFilter;
 using Recrutify.Services.DTOs;
 using Recrutify.Services.Services.Abstract;
