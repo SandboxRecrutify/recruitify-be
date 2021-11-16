1ff71da5845329df3b4304daa6a761777c880388
alesyalis <lis.alesia@mail.ru>
2021-11-16 22:45:36 +0300

    #54 add  [ODataAuthorize]

--- Recrutify/Recrutify.Host/Controllers/OData/ProjectsController.cs
+++ Recrutify/Recrutify.Host/Controllers/OData/ProjectsController.cs
@@ -24,7 +24,7 @@ namespace Recrutify.Host.Controllers.OData
         [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.Filter
             | AllowedQueryOptions.OrderBy | AllowedQueryOptions.Top
             | AllowedQueryOptions.Skip | AllowedQueryOptions.Count)]
-        //[ODataAuthorize]
+        [ODataAuthorize]
         [ODataRoute]
         public IQueryable<ProjectDTO> Get()
         {
