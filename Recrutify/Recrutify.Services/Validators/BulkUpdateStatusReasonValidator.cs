803e81aefcd8f9981d7bdf03ec93ea276f5ac2a2
alesyalis <lis.alesia@mail.ru>
2021-11-17 20:34:56 +0300

    add CandidatesAreExistingAsync into BulkUpdateStatusReasonValidator + rename bulkCreateTestFeedbackDTO

--- Recrutify/Recrutify.Services/Validators/BulkUpdateStatusReasonValidator.cs
+++ Recrutify/Recrutify.Services/Validators/BulkUpdateStatusReasonValidator.cs
@@ -1,16 +1,30 @@
 ﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using FluentValidation;
+using Recrutify.DataAccess.Models;
+using Recrutify.DataAccess.Repositories.Abstract;
 using Recrutify.Services.DTOs;
 
 namespace Recrutify.Services.Validators
 {
     public class BulkUpdateStatusReasonValidator : AbstractValidator<BulkUpdateStatusDTO>
     {
-        public BulkUpdateStatusReasonValidator()
+        private readonly ICandidateRepository _candidateRepository;
+
+        public BulkUpdateStatusReasonValidator(ICandidateRepository candidateRepository)
         {
-            RuleFor(x => x.CandidatesIds)
+            _candidateRepository = candidateRepository;
+
+            ConfigureRules();
+        }
+
+        private void ConfigureRules()
+        {
+            RuleFor(x => x)
                 .NotNull()
-                .NotEmpty();
+                .MustAsync(CandidatesAreExistingAsync)
+                .WithMessage("One or more candidates doesn't exist");
             RuleFor(x => x.Reason)
                 .NotNull()
                 .NotEmpty()
@@ -21,6 +35,17 @@ namespace Recrutify.Services.Validators
                 .WithMessage("Choose from existing statuses")
                 .Must(s => new[] { StatusDTO.Accepted, StatusDTO.Declined, StatusDTO.WaitingList, }.Contains(s))
                 .WithMessage("The selected status is not valid");
+        }
+
+        private async Task<bool> CandidatesAreExistingAsync(BulkUpdateStatusDTO dto, CancellationToken cancellationToken)
+        {
+            var candidates = await _candidateRepository.GetByIdsAsync(dto.CandidatesIds);
+            var filteredCandidatesIds = candidates.Where(c => c.ProjectResults
+                                                       .FirstOrDefault(p => p.ProjectId == dto.ProjectId)
+                                                       ?.Feedbacks.All(f => f.Type != FeedbackType.Test) ?? false)
+                                                   .Select(c => c.Id)
+                                                   .ToList();
+            return dto.CandidatesIds.All(id => filteredCandidatesIds.Contains(id));
         }
     }
 }
