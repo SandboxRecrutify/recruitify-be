e2c140c4e3b5a38794f35f896485232152970085
Evgen <evgentik_m@mail.ru>
2021-11-19 16:40:14 +0300

    GlobalSuppressions

--- Recrutify/Recrutify.DataAccess/GlobalSuppressions.cs
+++ Recrutify/Recrutify.DataAccess/GlobalSuppressions.cs
@@ -6,4 +6,3 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<Ожидание>", Scope = "member", Target = "~F:Recrutify.DataAccess.Repositories.Abstract.BaseRepository`1._filterBuilder")]
-[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1118:Parameter should not span multiple lines", Justification = "<Ожидание>", Scope = "member", Target = "~M:Recrutify.DataAccess.Repositories.UserRepository.GetByRoles(System.Collections.Generic.List{Recrutify.DataAccess.Models.Role})~System.Threading.Tasks.Task{System.Collections.Generic.IEnumerable{Recrutify.DataAccess.Models.User}}")]
