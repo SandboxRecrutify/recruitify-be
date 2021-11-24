5193e054f35dec33cddbbb7ce25b61af2f7afb6f
alesyalis <lis.alesia@mail.ru>
2021-11-24 17:59:42 +0300

    updating a project- staff is accepted only by id

--- Recrutify/Recrutify.Services/DTOs/ProjectDTO.cs
+++ Recrutify/Recrutify.Services/DTOs/ProjectDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Recrutify.DataAccess.Models;
 
 namespace Recrutify.Services.DTOs
 {
@@ -7,12 +8,12 @@ namespace Recrutify.Services.DTOs
     {
         public Guid Id { get; set; }
 
-        public IEnumerable<Guid> Managers { get; set; }
+        public IEnumerable<Staff> Managers { get; set; }
 
-        public IEnumerable<Guid> Interviewers { get; set; }
+        public IEnumerable<Staff> Interviewers { get; set; }
 
-        public IEnumerable<Guid> Recruiters { get; set; }
+        public IEnumerable<Staff> Recruiters { get; set; }
 
-        public IEnumerable<Guid> Mentors { get; set; }
+        public IEnumerable<Staff> Mentors { get; set; }
     }
 }
