b448149daac9c8f5bfd49d91f6ddfbac73bcce64
alesyalis <lis.alesia@mail.ru>
2021-11-25 21:53:51 +0300

    add ProjectPrimarySkillsAreExistingAsync, ProjectStartDateAreExistingAsync, ProjectNameAreExistingAsync into UpdateProjectValidator

--- Recrutify/Recrutify.Services/Validators/UpdateProjectValidator.cs
+++ Recrutify/Recrutify.Services/Validators/UpdateProjectValidator.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using FluentValidation;
-using Recrutify.DataAccess;
+using Recrutify.DataAccess.Repositories.Abstract;
 using Recrutify.Services.DTOs;
 
 namespace Recrutify.Services.Validators
 {
     public class UpdateProjectValidator : BaseProjectValidator<UpdateProjectDTO>
     {
-        public UpdateProjectValidator()
+        private readonly IProjectRepository _projectRepository;
+
+        public UpdateProjectValidator(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+
+            ConfigureRules();
+        }
+
+        private void ConfigureRules()
         {
             RuleFor(p => p.Id)
                 .NotEmpty();
@@ -16,9 +29,34 @@ namespace Recrutify.Services.Validators
                 .GreaterThanOrEqualTo(DateTime.UtcNow.Date)
                 .WithMessage("Date must be in the today or future!");
             RuleFor(p => p.Name)
-                .Null()
-                //.Equal(n => n.Name)
+                .MustAsync(ProjectNameAreExistingAsync)
                 .WithMessage("Name cannot be changed!");
+            RuleFor(p => p.StartDate)
+                .MustAsync(ProjectStartDateAreExistingAsync)
+                .WithMessage("StartDate cannot be changed!");
+            RuleFor(p => p.PrimarySkills)
+                .MustAsync(ProjectPrimarySkillsAreExistingAsync)
+                .WithMessage("PrimarySkills cannot be changed!");
+        }
+
+        private async Task<bool> ProjectNameAreExistingAsync(UpdateProjectDTO dto, string name, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetAsync(dto.Id);
+            return project.Name.Equals(name);
+        }
+
+        private async Task<bool> ProjectStartDateAreExistingAsync(UpdateProjectDTO dto, DateTime dateTime, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetAsync(dto.Id);
+            return project.StartDate.Equals(dateTime);
+        }
+
+        private async Task<bool> ProjectPrimarySkillsAreExistingAsync(UpdateProjectDTO dto, IEnumerable<ProjectPrimarySkillDTO> primariSkills, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetAsync(dto.Id);
+            var primariSkillIds = project.PrimarySkills.Select(p => p.Id).ToList();
+            var primariSkillIdsDto = primariSkills.Select(p => p.Id);
+            return primariSkillIds.SequenceEqual(primariSkillIdsDto);
         }
     }
 }
