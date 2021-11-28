f1fe44948a6599e9d5841c33efe051ec87e67f05
alesyalis <lis.alesia@mail.ru>
2021-11-29 00:43:02 +0300

    add BulkSendEmailsWithTestValidator

---
+++ Recrutify/Recrutify.Services/Validators/BulkSendEmailsWithTestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Recrutify.DataAccess.Repositories.Abstract;
+using Recrutify.Services.DTOs;
+
+namespace Recrutify.Services.Validators
+{
+    public class BulkSendEmailsWithTestValidator : AbstractValidator<BulkSendEmailWithTestDTO>
+    {
+        private readonly ICandidateRepository _candidateRepository;
+
+        public BulkSendEmailsWithTestValidator(ICandidateRepository candidateRepository)
+        {
+            _candidateRepository = candidateRepository;
+
+            ConfigureRules();
+        }
+
+        private void ConfigureRules()
+        {
+            RuleFor(x => x.CandidatesIds)
+                .NotNull()
+                .NotEmpty()
+                .MustAsync(_candidateRepository.ExistsByIdsAsync)
+                .WithMessage("One or more candidates doesn't exist");
+        }
+
+    }
+}
