9c739de469c8d92351deab43a9ee9a4642a281f9
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2021-12-02 18:22:52 +0300

    Add TimeSlotsForUserDTO

---
+++ Recrutify/Recrutify.Services/DTOs/TimeSlotsForUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recrutify.Services.DTOs
+{
+    public class TimeSlotsForUserDTO
+    {
+        public Guid UserId { get; set; }
+
+        public IEnumerable<DateTime> TimeSlots { get; set; }
+
+        public DateTime MondayDate { get; set; }
+    }
+}
