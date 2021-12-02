5ef4f4b932cc99150c0dfd09b1d66cbbf4c8b7eb
alesyalis <lis.alesia@mail.ru>
2021-12-02 14:56:00 +0300

    rename Odata SchedulesController

--- Recrutify/Recrutify.Host/Configuration/OData/ScheduleConfiguration.cs
+++ Recrutify/Recrutify.Host/Configuration/OData/ScheduleConfiguration.cs
@@ -8,13 +8,13 @@ namespace Recrutify.Host.Configuration
     {
         public void Apply(ODataModelBuilder builder, ApiVersion apiVersion, string routePrefix)
         {
-            builder.EntitySet<ScheduleCandidateInfoDTO>("Schedule");
+            builder.EntitySet<ScheduleCandidateInfoDTO>("Schedules");
             builder.EntityType<ScheduleCandidateInfoDTO>().Collection
                   .Function("GetCandidatesPassedTest")
-                  .ReturnsCollectionFromEntitySet<ScheduleCandidateInfoDTO>("Schedule");
+                  .ReturnsCollectionFromEntitySet<ScheduleCandidateInfoDTO>("Schedules");
             builder.EntityType<ScheduleCandidateInfoDTO>().Collection
                   .Function("GetUnassignedCandidates")
-                  .ReturnsCollectionFromEntitySet<ScheduleCandidateInfoDTO>("Schedule");
+                  .ReturnsCollectionFromEntitySet<ScheduleCandidateInfoDTO>("Schedules");
         }
     }
 }
