74a06bc3cc870e266fc5fd64c2723fe245fcd3b2
alesyalis <lis.alesia@mail.ru>
2021-12-05 23:18:24 +0300

    add

---
+++ Recrutify/Recrutify.Tests/Service/ProjectServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using Recrutify.DataAccess.Repositories.Abstract;
+using Recrutify.Services.Helpers.Abstract;
+using Recrutify.Services.Services;
+using Recrutify.Services.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recrutify.Tests.Service
+{
+    public class ProjectServiceTest
+    {
+        private ProjectService _projectService;
+        private Mock<IProjectRepository> _projectRepositoryMock;
+        private Mock<IUserService> _userServiceMock;
+        private Mock<IMapper> _mapperMock;
+        private Mock<IPrimarySkillService> _primarySkillMock;
+        private Mock<IStaffHelper> _staffHelperMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _projectRepositoryMock = new Mock<IProjectRepository>();
+            _userServiceMock = new Mock<IUserService>();
+            _mapperMock = new Mock<IMapper>();
+            _primarySkillMock = new Mock<IPrimarySkillService>();
+            _staffHelperMock = new Mock<IStaffHelper>();
+
+           /* _projectService = new ProjectService(
+               _projectRepositoryMock.Object,
+               _userServiceMock.Object,
+               _mapperMock.Object,
+               _primarySkillMock.Object,
+               _staffHelperMock.Object);*/
+        }
+
+
+
+    }
+}
