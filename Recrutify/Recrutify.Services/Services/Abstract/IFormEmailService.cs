29ca94a87e9e7ffbc1e1c153839e3e198a653d9b
alesyalis <lis.alesia@mail.ru>
2021-12-05 12:58:41 +0300

    update Test Email

--- Recrutify/Recrutify.Services/Services/Abstract/IFormEmailService.cs
+++ Recrutify/Recrutify.Services/Services/Abstract/IFormEmailService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Recrutify.Services.DTOs;
 using Recrutify.Services.EmailModels;
 
@@ -8,7 +9,7 @@ namespace Recrutify.Services.Services.Abstract
     {
         IEnumerable<EmailRequest> GetEmailRequestsForStatusChange(IEnumerable<CandidateDTO> candidates, ProjectDTO project, string templatePath);
 
-        IEnumerable<EmailRequest> GetEmailRequestsForSendTest(IEnumerable<CandidateDTO> candidates, ProjectDTO project);
+        IEnumerable<EmailRequest> GetEmailRequestsForSendTest(IEnumerable<CandidateDTO> candidates, ProjectDTO project, DateTime testDeadlineDate, string emailToContact);
 
         IEnumerable<EmailRequest> GetEmailRequestsForInterviewInvite(IEnumerable<Interview> interviews);
     }
